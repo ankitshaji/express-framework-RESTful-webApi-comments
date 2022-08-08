25f31497de39d948ff7d40ccb05d6e69833bdcbc
Ankit Shaji <ankitshaji@gmail.com>
2022-08-09 03:33:23 +1200

    adding fake database /resource

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ app.set("view engine", "ejs"); //auto require("ejs")
 //change path to absolute path to index.js
 app.set("views", path.join(__dirname, "/views"));
 
+//fake database /resource
+//array of objects
+const comments = [
+  {
+    username: "Todd",
+    comment: "lol that is so funny",
+  },
+  {
+    username: "Skyler",
+    comment: "I like to go bird watching with my dog",
+  },
+  {
+    username: "Sk8rboi",
+    comment: "Plz delete your account,Todd",
+  },
+  {
+    username: "onlysaywoof",
+    comment: "woof woof woof",
+  },
+];
+
 //executes callback
 app.listen(3000, () => {
   console.log("listning on port 3000;");
